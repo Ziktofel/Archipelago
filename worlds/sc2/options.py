5243cc5373dd03cd252e45a71930686f0732b96a
Ziktofel <ziktofel@gmail.com>
2025-09-29 00:26:30 +0200

    Add possible values to campaign list for YAMLs

--- worlds/sc2/options.py
+++ worlds/sc2/options.py
@@ -251,7 +251,17 @@ class PlayerColorNova(ColorChoice):
 
 
 class EnabledCampaigns(OptionSet):
-    """Determines which campaign's missions will be used"""
+    """
+    Determines which campaign's missions will be used
+    Valid keys:
+    - Wings of Liberty
+    - Prophecy
+    - Heart of the Swarm
+    - Whispers of Oblivion (Legacy of the Void: Prologue)
+    - Legacy of the Void
+    - Into the Void (Legacy of the Void: Epilogue)
+    - Nova Covert Ops
+    """
     display_name = "Enabled Campaigns"
     valid_keys = {campaign.campaign_name for campaign in SC2Campaign if campaign != SC2Campaign.GLOBAL}
     default = valid_keys
